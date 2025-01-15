cd424c9358dae1770cd8eabf8bbc00039474b9d9
siruoren <zhuguojun6@163.com>
2025-01-15 16:52:59 +0800

    Update iptv_to_sql.sh

--- iptv_to_sql.sh
+++ iptv_to_sql.sh
@@ -48,12 +48,13 @@ echo "check duochang......"
 
 cd duochang;
 > duochang_update.sql;
+> duochang_update.sqltmp;
 cat api.list|while read line;
 do
     api_name=`echo -n ${line}|awk '{print$1}'`
     api_url=`echo -n ${line}|awk '{print$2}'`
 	
-	if [ "${api_name}" == ""|| "${api_url}" == "" ];then
+	if [ "${api_name}" == "" ] || [ "${api_url}" == "" ];then
 	   continue
 	fi
 	
