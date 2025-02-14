6eeeda387f0a9d7bba2f55c462da1305216e2848
Your Name <you@example.com>
2025-02-14 11:10:48 +0800

    update

--- iptv_to_sql.sh
+++ iptv_to_sql.sh
@@ -39,8 +39,8 @@ done
 > IPTV_update.sql;
 echo "set character_set_server='utf8';" >> IPTV_update.sql;
 echo "TRUNCATE table tvbox.tv_channels;" >> IPTV_update.sql;
-cat IPTV_update.sqltmp|grep '总台' >> IPTV_update.sql;
-cat IPTV_update.sqltmp|grep -v '总台' >> IPTV_update.sql;
+cat IPTV_update.sqltmp|grep -iE "总台|央视" >> IPTV_update.sql;
+cat IPTV_update.sqltmp|grep -ivE "总台|央视" >> IPTV_update.sql;
 rm -f IPTV_update.sqltmp;
 cd ../;
 
