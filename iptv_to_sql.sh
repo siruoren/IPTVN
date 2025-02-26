c94808efc7a9308251038079ed433b78e1491c7d
Your Name <you@example.com>
2025-02-26 17:07:19 +0800

    add src_list

--- iptv_to_sql.sh
+++ iptv_to_sql.sh
@@ -71,7 +71,7 @@ fi
     		      item_id=`sed -n "${line_nu}p" ${m3u_file}|awk -F'tvg-name=' '{print$2}'|awk '{printf$1}' |sed 's/"//g'`
     			    item_group=`sed -n "${line_nu}p" ${m3u_file}|awk -F'group-title=' '{print$2}'|awk '{printf$1}' |sed 's/"//g'|awk -F',' '{printf$1}'|sed 's/[^[:alpha:]]//g'`
     			    item_url=`sed -n "${line_next}p" ${m3u_file}|grep '^http'`
-    			    echo "INSERT into tvbox.tv_channels(name,category,url) value('${item_id}','${item_group}','${item_url}');" >> IPTV_update.sqltmp
+    			    echo "INSERT into tvbox.tv_channels(name,category,url) (select '${item_id}','${item_group}','${item_url}' from tvbox.tv_channels where not EXISTS(select url from tvbox.tv_channels where url='${item_url}'));" >> IPTV_update.sqltmp
     			    i=` expr $i + 1 `
     	    fi
     	fi
@@ -83,6 +83,7 @@ done
 > IPTV_update.sql;
 echo "set character_set_server='utf8';" >> IPTV_update.sql;
 echo "TRUNCATE table tvbox.tv_channels;" >> IPTV_update.sql;
+echo "SELECT SLEEP(5);" >> IPTV_update.sql;
 cat IPTV_update.sqltmp|sort|uniq|grep -iE "总台|央视" >> IPTV_update.sql;
 cat IPTV_update.sqltmp|sort|uniq|grep -ivE "总台|央视" >> IPTV_update.sql;
 default_assign_all="${default_assign_first}${default_assign_second}"
