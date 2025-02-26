f02191047d1e51f972ca588eb7081da66fdcc46f
Your Name <you@example.com>
2025-02-26 13:21:36 +0800

    update

--- iptv_to_sql.sh
+++ iptv_to_sql.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # 源
-rm -f IPTV.m3u 
-wget https://raw.githubusercontent.com/Guovin/iptv-api/gd/output/result.m3u -O IPTV.m3u
+> IPTV.m3u 
+wget https://raw.githubusercontent.com/Guovin/iptv-api/gd/output/result.m3u -O Guovin.m3uu;cat Guovin.m3u >> IPTV.m3u;rm -f Guovin.m3u;
 wget https://raw.githubusercontent.com/fanmingming/live/main/tv/m3u/ipv6.m3u -O fangming.m3u;cat fangming.m3u >> IPTV.m3u;rm -f fangming.m3u;
 wget https://raw.githubusercontent.com/vbskycn/iptv/refs/heads/master/tv/iptv4.m3u -O vbskycn_iptv4.m3u;cat vbskycn_iptv4.m3u >> IPTV.m3u;rm -f vbskycn_iptv4.m3u;
 wget https://raw.githubusercontent.com/vbskycn/iptv/refs/heads/master/tv/iptv6.m3u -O vbskycn_iptv6.m3u;cat vbskycn_iptv6.m3u >> IPTV.m3u;rm -f vbskycn_iptv6.m3u;
