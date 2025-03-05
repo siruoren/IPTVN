48a71fc62a08f368ccda1809bbc274eb11f32383
Your Name <you@example.com>
2025-03-05 17:14:27 +0800

    update

--- scripts/analyse_json.py
+++ scripts/analyse_json.py
@@ -7,7 +7,7 @@ def main(url):
     response = urllib.request.urlopen(url)
     data = json.load(response)
 
-    for list in data:
+    for list in data['urls']:
         print(list['name']+ ' ' + list['url'])
 
 
