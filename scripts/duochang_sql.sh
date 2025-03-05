a7e9c1d3d6c47021147368ab9bb2e59563793b87
Your Name <you@example.com>
2025-03-05 18:09:16 +0800

    update

--- scripts/duochang_sql.sh
+++ scripts/duochang_sql.sh
@@ -80,7 +80,7 @@ check_res=`cat duochang_update.sqltmp|sort|uniq|wc -l`
 if [ "${check_res}" -ne "0" ];then
 
   echo "set character_set_server='utf8';" >> duochang_update.sql;
-  echo "UPDATE tvbox.tv_app SET appkey = 'bef838a270105a93935038c844192fd3' WHERE name = '群晖影视';" >> duochang_update.sql;
+  #echo "UPDATE tvbox.tv_app SET appkey = 'bef838a270105a93935038c844192fd3' WHERE name = '群晖影视';" >> duochang_update.sql;
   cat duochang_update.sqltmp|sort|uniq >> duochang_update.sql;
 fi
   rm -f duochang_update.sqltmp;
