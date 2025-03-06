ed394e1a7aafc37a88499aebc6bb997ca3589a9a
Your Name <you@example.com>
2025-03-06 18:07:21 +0800

    update

--- scripts/iptv_to_sql.sh
+++ scripts/iptv_to_sql.sh
@@ -70,7 +70,7 @@ fi
     	    if [[ "${check_url}" -ne 0 ]];then
     		      item_id=`sed -n "${line_nu}p" ${m3u_file}|awk -F'tvg-name=' '{print$2}'|awk '{printf$1}' |sed 's/"//g'`
     			    item_group=`sed -n "${line_nu}p" ${m3u_file}|awk -F'group-title=' '{print$2}'|awk '{printf$1}' |sed 's/"//g'|awk -F',' '{printf$1}'|sed 's/[^[:alpha:]]//g'`
-    			    item_url=`sed -n "${line_next}p" ${m3u_file}|grep '^http'|sed 's#\\.*##g'`
+    			    item_url=`sed -n "${line_next}p" ${m3u_file}|grep '^http'|sed 's#\\.*##g'|sed 's#\\##g'`
     			    echo "INSERT into tvbox.tv_channels(name,category,url) select '${item_id}','${item_group}','${item_url}' where NOT EXISTS (SELECT 1 FROM tvbox.tv_channels WHERE url = '${item_url}');" >> IPTV_update.sqltmp
     			    i=` expr $i + 1 `
     	    fi
