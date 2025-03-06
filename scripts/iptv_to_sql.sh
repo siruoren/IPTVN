8f46129cb7d773b315d9561640ad9dbee1b4cb33
Your Name <you@example.com>
2025-03-06 18:18:32 +0800

    update

--- scripts/iptv_to_sql.sh
+++ scripts/iptv_to_sql.sh
@@ -70,7 +70,7 @@ fi
     	    if [[ "${check_url}" -ne 0 ]];then
     		      item_id=`sed -n "${line_nu}p" ${m3u_file}|awk -F'tvg-name=' '{print$2}'|awk '{printf$1}' |sed 's/"//g'`
     			    item_group=`sed -n "${line_nu}p" ${m3u_file}|awk -F'group-title=' '{print$2}'|awk '{printf$1}' |sed 's/"//g'|awk -F',' '{printf$1}'|sed 's/[^[:alpha:]]//g'`
-    			    item_url=`sed -n "${line_next}p" ${m3u_file}|grep '^http'|sed 's#\\.*##g'|sed 's#\\##g'`
+    			    item_url=`sed -n "${line_next}p" ${m3u_file}|grep '^http'|sed 's#\\.*##g'`
     			    echo "INSERT into tvbox.tv_channels(name,category,url) select '${item_id}','${item_group}','${item_url}' where NOT EXISTS (SELECT 1 FROM tvbox.tv_channels WHERE url = '${item_url}');" >> IPTV_update.sqltmp
     			    i=` expr $i + 1 `
     	    fi
@@ -85,8 +85,8 @@ echo "set character_set_server='utf8';" >> IPTV_update.sql;
 #echo "TRUNCATE table tvbox.tv_channels;" >> IPTV_update.sql;
 echo "INSERT into tvbox.tv_channels(name,category,url) values('default', 'default', 'default');" >> IPTV_update.sql;
 echo "SELECT SLEEP(5);" >> IPTV_update.sql;
-cat IPTV_update.sqltmp|grep -iE "总台|央视" >> IPTV_update.sql;
-cat IPTV_update.sqltmp|grep -ivE "总台|央视" >> IPTV_update.sql;
+cat IPTV_update.sqltmp|grep -iE "总台|央视"|grep -v '\\' >> IPTV_update.sql;
+cat IPTV_update.sqltmp|grep -ivE "总台|央视"|grep -v '\\' >> IPTV_update.sql;
 echo "DELETE FROM tvbox.tv_channels where name='default';" >> IPTV_update.sql;
 default_assign_all="${default_assign_first}${default_assign_second}"
 
