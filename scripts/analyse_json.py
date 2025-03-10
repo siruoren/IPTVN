d910ce35d56460b260c207f63748b64a0326f719
Your Name <you@example.com>
2025-03-10 17:32:22 +0800

    update

--- scripts/analyse_json.py
+++ scripts/analyse_json.py
@@ -4,14 +4,13 @@ import json
 import sys
 
 def main(url):
-    try:
-        response = urllib.request.urlopen(url)
-        data = json.load(response)
-        sum_data = json.load(duochang_file)
-        for list in data['urls']:
-            print(list['name']+ ' ' + list['url'])
-    except :
-        continue
+
+    response = urllib.request.urlopen(url)
+    data = json.load(response)
+    sum_data = json.load(duochang_file)
+    for list in data['urls']:
+        print(list['name']+ ' ' + list['url'])
+
 
 item=sys.argv[1]
 main(item)
