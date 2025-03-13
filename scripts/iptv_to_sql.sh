cfd31fedc055714ffced165bb5c468aefb0d6d17
Your Name <you@example.com>
2025-03-13 17:51:07 +0800

    update

--- scripts/iptv_to_sql.sh
+++ scripts/iptv_to_sql.sh
@@ -85,7 +85,7 @@ echo "set character_set_server='utf8';" >> IPTV_update.sql;
 echo "TRUNCATE table tvbox.tv_channels;" >> IPTV_update.sql;
 echo "INSERT into tvbox.tv_channels(name,category,url) values('default', 'default', 'default');" >> IPTV_update.sql;
 echo "SELECT SLEEP(5);" >> IPTV_update.sql;
-cat IPTV_update.sqltmp|grep -iE "总台|央视"|grep -v '\\' >> IPTV_update.sql;
+cat IPTV_update.sqltmp|grep -iE "总台|央视"|grep -v '\\'|grep -v liveshow >> IPTV_update.sql;
 cat IPTV_update.sqltmp|grep -ivE "总台|央视"|grep -v '\\' >> IPTV_update.sql;
 echo "DELETE FROM tvbox.tv_channels where name='default';" >> IPTV_update.sql;
 default_assign_all="${default_assign_first}${default_assign_second}"
