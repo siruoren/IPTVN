373bf141bc3a49a0199e439db7aebf17852e95bf
Your Name <you@example.com>
2025-04-17 11:26:21 +0800

    rename

--- scripts/analyse_json.py
+++ scripts/analyse_json.py
@@ -7,13 +7,13 @@ def main(url):
     dc_file = open('duocang.json','r',encoding='utf-8')
     file_data=dc_file.read()
     duochang_json=json.loads(file_data)
-    if 'storeHouse' not in duochang_json:
-        duochang_json['storeHouse'] = []
+    if 'urls' not in duochang_json:
+        duochang_json['urls'] = []
     response = urllib.request.urlopen(url,timeout=10.0)
     data = json.load(response)
     for list in data['urls']:
         print(list['name']+ ' ' + list['url'])
-        duochang_json['storeHouse'].append(list)
+        duochang_json['urls'].append(list)
     with open('duocang.json','w',encoding='utf-8') as file:
         json.dump(duochang_json,file,ensure_ascii=False,indent=4)
 
