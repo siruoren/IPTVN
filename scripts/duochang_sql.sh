cfc86e21d8558faa1da01fe23583720ecdd3bc96
Your Name <you@example.com>
2025-04-17 10:58:11 +0800

    rename

--- scripts/duochang_sql.sh
+++ scripts/duochang_sql.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 cd $(dirname $0);
 cd ../;
-echo "check duochang......"
+echo "check duocang......"
 
-cd duochang;
-> duochang_update.sql;
-> duochang_update.sqltmp;
-> duochang.listtmp;
+cd duocang;
+> duocang_update.sql;
+> duocang_update.sqltmp;
+> duocang.listtmp;
 echo '{}'> duocang.json;
 
-#duochangjuhe
+#duocangjuhe
 cat juhe.list|while read line;
 do
     api_name=`echo -n ${line}|awk '{print$1}'`
@@ -18,14 +18,14 @@ do
     	if [ "${api_name}" == "" ] || [ "${api_url}" == "" ];then
 	   continue
 	fi
-    python ../scripts/analyse_json.py ${api_url} 'add' >>duochang.listtmp;
+    python ../scripts/analyse_json.py ${api_url} 'add' >>duocang.listtmp;
 
 
 done
 
-sed -i 's#https://ghproxy.net/##g' duochang.listtmp;
+sed -i 's#https://ghproxy.net/##g' duocang.listtmp;
 
-cat duochang.listtmp|sort|uniq|while read line;
+cat duocang.listtmp|sort|uniq|while read line;
 do
     api_name=`echo -n ${line}|awk '{print$1}'|sed 's/[^[:alpha:]]//g'`
     api_url=`echo -n ${line}|awk '{print$2}'`
@@ -40,7 +40,7 @@ do
     if [[ $response -gt 10  ]]; then
       echo "URL is accessible"
 
-      echo "INSERT into tvbox.tv_app_duocang(name, url, appid, status, status_dcjm) select '${api_name}','${api_url}','10000','y','n' where NOT EXISTS (SELECT 1 FROM tvbox.tv_app_duocang WHERE name = '${api_name}');" >> duochang_update.sqltmp
+      echo "INSERT into tvbox.tv_app_duocang(name, url, appid, status, status_dcjm) select '${api_name}','${api_url}','10000','y','n' where NOT EXISTS (SELECT 1 FROM tvbox.tv_app_duocang WHERE name = '${api_name}');" >> duocang_update.sqltmp
 
 
 
@@ -49,7 +49,7 @@ do
     fi
 done
 
- rm -f duochang.listtmp;
+ rm -f duocang.listtmp;
 
 
 cat api.list|while read line;
@@ -68,7 +68,7 @@ do
 
       echo "URL is accessible"
 
-      echo "INSERT into tvbox.tv_app_duocang(name, url, appid, status, status_dcjm) select '${api_name}','${api_url}','10000','y','n' where NOT EXISTS (SELECT 1 FROM tvbox.tv_app_duocang WHERE name = '${api_name}');" >> duochang_update.sqltmp
+      echo "INSERT into tvbox.tv_app_duocang(name, url, appid, status, status_dcjm) select '${api_name}','${api_url}','10000','y','n' where NOT EXISTS (SELECT 1 FROM tvbox.tv_app_duocang WHERE name = '${api_name}');" >> duocang_update.sqltmp
 
 
 
@@ -77,15 +77,15 @@ do
     fi
 done
 
-check_res=`cat duochang_update.sqltmp|sort|uniq|wc -l`
+check_res=`cat duocang_update.sqltmp|sort|uniq|wc -l`
 
 if [ "${check_res}" -ne "0" ];then
 
-  echo "set character_set_server='utf8';" >> duochang_update.sql;
-  #echo "UPDATE tvbox.tv_app SET appkey = 'bef838a270105a93935038c844192fd3' WHERE name = '群晖影视';" >> duochang_update.sql;
-  cat duochang_update.sqltmp|sort|uniq >> duochang_update.sql;
+  echo "set character_set_server='utf8';" >> duocang_update.sql;
+  #echo "UPDATE tvbox.tv_app SET appkey = 'bef838a270105a93935038c844192fd3' WHERE name = '群晖影视';" >> duocang_update.sql;
+  cat duocang_update.sqltmp|sort|uniq >> duocang_update.sql;
 fi
-  rm -f duochang_update.sqltmp;
+  rm -f duocang_update.sqltmp;
 
 
 cd ../;
