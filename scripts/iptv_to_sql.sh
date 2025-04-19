a648075c6b9a509fc9b31c3367652ed7acb1887f
Your Name <you@example.com>
2025-04-19 14:05:34 +0800

    add

--- scripts/iptv_to_sql.sh
+++ scripts/iptv_to_sql.sh
@@ -109,11 +109,11 @@ fi
     		      item_id=`sed -n "${line_nu}p" ${m3u_file}|awk -F'tvg-name=' '{print$2}'|awk '{printf$1}' |sed 's/"//g'`
     			    item_group=`sed -n "${line_nu}p" ${m3u_file}|awk -F'group-title=' '{print$2}'|awk '{printf$1}' |sed 's/"//g'|awk -F',' '{printf$1}'|sed 's/[^[:alpha:]]//g'`
     			    item_url=`sed -n "${line_next}p" ${m3u_file}|grep '^http'`
-           url_res=''
+                    url_res=''
                     if [[ "${default_assign_first}" =~ "${item_group}" ]];then
                         if [[ "${item_url}" =~ '\[' ]];then
-                            #url_res=`curl -6 -o /dev/null -s -w "%{http_code}" --max-time 3 "${item_url}"`
-                            url_res='skip'
+                            url_res=`curl -6 -o /dev/null -s -w "%{http_code}" --max-time 3 "${item_url}"`
+                            #url_res='skip'
                         else
                             url_res=`curl -o /dev/null -s -w "%{http_code}" --max-time 3 "${item_url}"`
                         fi
