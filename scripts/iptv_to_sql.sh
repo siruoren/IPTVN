7683574e9fb3fefdb48f4d22d425af14f7e6dafb
siruoren <zhuguojun6@163.com>
2025-04-19 12:27:49 +0800

    更新 iptv_to_sql.sh

--- scripts/iptv_to_sql.sh
+++ scripts/iptv_to_sql.sh
@@ -109,6 +109,7 @@ fi
     		      item_id=`sed -n "${line_nu}p" ${m3u_file}|awk -F'tvg-name=' '{print$2}'|awk '{printf$1}' |sed 's/"//g'`
     			    item_group=`sed -n "${line_nu}p" ${m3u_file}|awk -F'group-title=' '{print$2}'|awk '{printf$1}' |sed 's/"//g'|awk -F',' '{printf$1}'|sed 's/[^[:alpha:]]//g'`
     			    item_url=`sed -n "${line_next}p" ${m3u_file}|grep '^http'`
+           url_res=''
                     if [[ "${default_assign_first}" =~ "${item_group}" ]];then
                         if [[ "${item_url}" =~ '\[' ]];then
                             #url_res=`curl -6 -o /dev/null -s -w "%{http_code}" --max-time 3 "${item_url}"`
