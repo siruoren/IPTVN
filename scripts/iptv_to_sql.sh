d7b87e9bc5297aa1db0f2cc526e6e540a77cd906
siruoren <zhuguojun6@163.com>
2025-04-25 21:37:52 +0800

    更新 iptv_to_sql.sh

--- scripts/iptv_to_sql.sh
+++ scripts/iptv_to_sql.sh
@@ -7,7 +7,7 @@ mkdir -p ../IPTV;
 
 rm -rf ../IPTV/*.m3u;
 
-while read extend_line|| [[ ! -n $extend_line ]]
+while read extend_line|| [ ! -n $extend_line ]
 do
 if [ "${extend_line}" != '' ];then
     echo ${extend_line}
@@ -15,7 +15,7 @@ if [ "${extend_line}" != '' ];then
     src_url=`echo $extend_line|awk '{print$2}'`
     wget ${src_url} -O ${src_name}.m3u;
     extend_group_name=''
-    while read line|| [[ ! -n $line ]]
+    while read line|| [ ! -n $line ]
     do
             if [[ ${line} =~ '#genre#' ]];then
                             extend_group_name=`echo ${line}|grep '#genre#'|awk -F',' '{print$1}'`
@@ -42,7 +42,7 @@ done < ../IPTV/iptv_src_extend.list;
 
 
 
-while read line|| [[ ! -n $line ]]
+while read line|| [ ! -n $line ]
 do
 
 src_name=`echo $line|awk '{print$1}'`
