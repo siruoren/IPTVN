6429ddb0ae0f22e15b4b6986b8b87a3b8fb169c5
Your Name <you@example.com>
2025-07-04 15:46:45 +0800

    up

--- scripts/iptv_check.sh
+++ scripts/iptv_check.sh
@@ -1,4 +1,5 @@
 cd $(dirname $0);
 cd ../IPTV-M3U-Checker2;
-pip3 install --user -r requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple;
+#pip3 install --user -r requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple;
+pip3 install --user -r requirements.txt;
 python3 main.py
